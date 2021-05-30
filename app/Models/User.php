6ce70be1385891033827cef068062ad366c673c2
Julius Ceasar <joks026@gmail.com>
2021-05-30 23:08:51 +0800

    database fake datas

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
+
 
 class User extends Authenticatable
 {
@@ -47,10 +47,6 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    public function getTitleAttribute($value)
-    {
-       $this->attributes['title'] = $value;
-       $this->attributes['slug'] = str::slug($value);
-    }
+
 
 }
